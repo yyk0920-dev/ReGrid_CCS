6f795bb7592478fdde9667d295b4c82ce1fd06bc
yyk0920-dev <yyk0920_@kookmin.ac.kr>
2026-05-13 12:36:08 +0900

    Clean CCS build files

--- CPU1_FLASH/objects.mk
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-USER_OBJS :=
-
-LIBS := -lc2000ware_libraries.cmd.genlibs -lboard.cmd.genlibs -ldevice_cmd.cmd.genlibs -llibc.a
-
